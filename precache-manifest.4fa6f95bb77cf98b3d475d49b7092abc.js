1ffa2a3d8eabaf8a480a719a48a7832dac73f3cb
Erik Sanders <alexreals78@gmail.com>
2022-05-31 22:40:21 +0300

    Hello

---
+++ precache-manifest.4fa6f95bb77cf98b3d475d49b7092abc.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64d8c7bce9e47d0946e4a51c7b3e2e80",
+    "url": "/MusicApp/assets/img/header.png"
+  },
+  {
+    "revision": "48025182349af4abfe3e272a7e128c4c",
+    "url": "/MusicApp/assets/img/introduction-music.png"
+  },
+  {
+    "revision": "90840bfca190b45f7cd04ed9d8c2cd54",
+    "url": "/MusicApp/assets/img/introduction-users.png"
+  },
+  {
+    "revision": "eba639c1b0a9287dd3a1904b9ae9831f",
+    "url": "/MusicApp/assets/img/profile-pic.jpg"
+  },
+  {
+    "revision": "d899ba7d2cac3f11c4377a700d68d22e",
+    "url": "/MusicApp/assets/img/song-header.png"
+  },
+  {
+    "revision": "e7b9aeb02c86a922404d13073d21836a",
+    "url": "/MusicApp/assets/img/user-header.png"
+  },
+  {
+    "revision": "4fc5530d90c49762233c",
+    "url": "/MusicApp/css/app.5abe1aa6.css"
+  },
+  {
+    "revision": "fe2b6bb561a871da1a64",
+    "url": "/MusicApp/css/chunk-2f5ece48.b476604a.css"
+  },
+  {
+    "revision": "27f4571521292a0d464b",
+    "url": "/MusicApp/css/chunk-vendors.06a7122d.css"
+  },
+  {
+    "revision": "7b7f5778aa2ab8a07a8a66fe41509b78",
+    "url": "/MusicApp/index.html"
+  },
+  {
+    "revision": "4fc5530d90c49762233c",
+    "url": "/MusicApp/js/app.6bf2b4ae.js"
+  },
+  {
+    "revision": "fe2b6bb561a871da1a64",
+    "url": "/MusicApp/js/chunk-2f5ece48.4af59425.js"
+  },
+  {
+    "revision": "81dd96b0732f9d3cd2de",
+    "url": "/MusicApp/js/chunk-43c06e80.4c08cc94.js"
+  },
+  {
+    "revision": "27f4571521292a0d464b",
+    "url": "/MusicApp/js/chunk-vendors.04e57bdb.js"
+  },
+  {
+    "revision": "b0b7ea903fe9f45583a2",
+    "url": "/MusicApp/js/groupedChunk.79231cad.js"
+  },
+  {
+    "revision": "f7216665154ef9db8b6fb3462c035e27",
+    "url": "/MusicApp/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/MusicApp/robots.txt"
+  }
+]);
